44b2fa7e477b224f4aaed520a56883b542ca6c5f
Nokocchi <59347338+Nokocchi@users.noreply.github.com>
2023-07-23 19:13:59 +0200

    Proof of concept, stuff works?!

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,38 @@
 <script lang="ts">
-  let count: number = 0
-  const increment = () => {
-    count += 1
-  }
+    import Test from "./Test.svelte";
+    let count:number = 1;
+
+    function increment(){
+    if(count < 250){
+      count++;
+    } else {
+      count = 1;
+    }
+}
+
+    function decrement(){
+      if(count > 1){
+      count--;
+    } else {
+      count = 251;
+    }
+}
+
 </script>
 
-<button on:click={increment}>
-  count is equal to {count}
+<Test pkmnNum={count}/>
+<div>
+<button on:click={decrement}>
+  Click here to cycle backwards pokemons! 
 </button>
+<button on:click={increment}>
+  Click here to cycle forwards pokemons! 
+</button>
+</div>
+
+<style>
+  .flexthing {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
---
+++ src/lib/Test.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+export let pkmnNum: number = 1;
+
+
+</script>
+
+<div>
+<img src="/crystal/{pkmnNum}.png" alt="img"/>
+</div>
